4a8a8354a307c9332e8fec82975730e0aae0c18b
Miroslav Peev <m1r0n4ouz@gmail.com>
2024-11-25 18:45:26 +0200

    Install tailwind/daisyui/fontAwesome plus configuration and tests

--- client/lacedUp/src/app/app.component.ts
+++ client/lacedUp/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaConfig, FaIconLibrary , FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'lacedUp';
+export class AppComponent implements OnInit {
+
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig);
+
+
+  ngOnInit(): void {
+    this.initFontAwesome();  
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far'
+    this.faIconLibrary.addIcons(...fontAwesomeIcons)
+  }
 }
