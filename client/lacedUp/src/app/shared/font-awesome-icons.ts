7ba6f07df6dfb07237182396b35e6b427e4fc71a
Miroslav Peev <m1r0n4ouz@gmail.com>
2024-11-26 02:54:15 +0200

    Add footer

--- client/lacedUp/src/app/shared/font-awesome-icons.ts
+++ client/lacedUp/src/app/shared/font-awesome-icons.ts
@@ -1,7 +1,12 @@
 import {IconDefinition} from '@fortawesome/angular-fontawesome'
-import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping, faTruckFast, faUser } from '@fortawesome/free-solid-svg-icons'
+import {faYoutube , faInstagram, faTwitter} from '@fortawesome/free-brands-svg-icons'
 
 export const fontAwesomeIcons: IconDefinition[] = [
     faUser,
     faCartShopping,
+    faTruckFast,
+    faYoutube,
+    faTwitter,
+    faInstagram,
 ]
